5279be4be5b884443a1c6caecfa1dc115888d455
William Violette <violettewj@gmail.com>
2019-05-07 10:59:12 -0600

    updated functionality of timepicker

--- timepickertest1/src/Built.js
+++ timepickertest1/src/Built.js
@@ -4,28 +4,65 @@ class Built extends Component {
     state = {
         hour: 12,
         minute: Number('00'),
+        hrdawn: "AM",
+        hrdusk: "PM",
+        PM: false
 
     }
 
-    incrementHour = () => {
-        this.setState(prevState => ({
+    incrementHour = () => { 
+        if(this.state.hour === 12){
+            this.setState({
+                hour: 1,
+                PM: !this.state.pm
+            })
+        }
+        else if(this.state.hour <= 11 && this.state.hour >= 0) {this.setState(prevState => ({
             hour: prevState.hour + 1
-        }))}
+        })
+        )
+    } 
+}
+
+    
 
         decrementHour = () => {
-            this.setState(prevState => ({
-                hour: prevState.hour - 1
-            }))}
+            if(this.state.hour === 1){
+                this.setState({
+                    hour: 12
+                })
+            } else if(this.state.hour <= 12 || this.state.hour > 0){
+                this.setState(prevState => ({
+                    hour: prevState.hour - 1
+                })
+                )
+            }
+        }
+            
 
     incrementMin = () => {
+        console.log('hello')
+        if(this.state.minute < 59 && this.state.minute >= 0) {
         this.setState(prevState => ({
             minute: prevState.minute + 1
-        }))
+        })
+        )
+    }
+        else if(this.state.minute === 59){
+           this.incrementHour()
+           this.resetMinutes()
+        }
     }
 
     decrementMin = () => {
         this.setState(prevState => ({
             minute: prevState.minute - 1
+        }))
+    }
+
+    resetMinutes = () => {
+        this.setState(() => ({
+            minute: 0
         }))
     }
 
@@ -48,11 +85,11 @@ class Built extends Component {
                 <button onClick={this.decrementMin}>
                     <img src={require('./img/downarrow.png')} alt="downMin"/>
                 </button>
+                <span style={{fontSize: 20}}>{this.state.PM === false ? this.state.hrdawn : this.state.hrdusk }</span>
             </div>
 
         )
     }
-
 }
 
 export default Built
